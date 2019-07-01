2c77b5777f6d53594f26994c81d9aca1ced312aa
Adi Priyanto <adipriyanto023@gmail.com>
2019-07-01 16:45:54 +0700

    lat command pattern

---
+++ Minggu3/hari1/lat/Circle.java
@@ -0,0 +1,7 @@
+public class Circle implements Shape{
+
+   
+   public void draw() {
+      System.out.println("Inside Circle::draw() method.");
+   }
+}
---
+++ Minggu3/hari1/lat/Rectangle.java
@@ -0,0 +1,6 @@
+public class Rectangle implements Shape{
+	
+	public void draw(){
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+} 
